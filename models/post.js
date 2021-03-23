40a4988d4bfac6c4e74526a5f03db1dcb5972eb2
Emincan Demirkaya <emincan@fireflyon.com>
2021-03-23 19:42:12 +0300

    add tag and image

--- models/post.js
+++ models/post.js
@@ -4,6 +4,8 @@ const postSchema = mongoose.Schema({
   title: String,
   subtitle: String,
   content: String,
+  tag: String,
+  image: String,
   createdAt: {
     type: Date,
     default: new Date(),
